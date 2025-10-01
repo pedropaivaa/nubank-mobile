f69fbcaf83648a78ef8a687850a845469e86ea53
PEDRO HENRIQUE PAIVA DOS SANTOS <2023334690@MACEIO.IFAL>
2025-10-01 15:20:20 -0300

    feat: start main

---
+++ nubank/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image source={require('./assets/logo.png')}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#8a05be',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
